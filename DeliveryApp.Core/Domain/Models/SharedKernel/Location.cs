3cc64301bf9d1d4e9106cf29b21c6e52f8d750e5
Valery Samutin <valsalery@gmail.com>
2025-06-06 23:57:32 +0300

    Добавление Location и покрытие тестами

---
+++ DeliveryApp.Core/Domain/Models/SharedKernel/Location.cs
@@ -0,0 +1,101 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Core.Domain.Models.SharedKernel
+{
+    /// <summary>
+    /// Координата
+    /// </summary>
+    public class Location : ValueObject
+    {
+        /// <summary>
+        /// Константа с минимальным значением 
+        /// </summary>
+        private const int MinValue = 1;
+
+        /// <summary>
+        /// Константа с максимальным значением 
+        /// </summary>
+        private const int MaxValue = 10;
+
+        /// <summary>
+        /// Сообщение при ошибке валидации значения
+        /// </summary>
+        private const string ErrorMessage = "Значение для {0} может быть в диапазоне от 1 до 10 включительно";
+
+        /// <summary>
+        /// Ctr
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        [ExcludeFromCodeCoverage]
+        private Location() { }
+
+
+        /// <summary>
+        /// Ctr
+        /// </summary>
+        /// <param name="x">Координата по оси X</param>
+        /// <param name="y">Координата по оси Y</param>
+        private Location(byte x, byte y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Координата X
+        /// </summary>
+        public byte X { get; }
+
+        /// <summary>
+        /// Координата Y
+        /// </summary>
+        public byte Y { get; }
+
+        /// <summary>
+        /// Factory Method
+        /// </summary>
+        /// <param name="x">Координата по оси X</param>
+        /// <param name="y">Координата по оси Y</param>
+        /// <returns>Результат</returns>
+        public static Result<Location, Error> Create(byte x, byte y)
+        {
+
+            if (x < MinValue || x > MaxValue)
+            {
+                return GeneralErrors.ValueIsInvalid(string.Format(ErrorMessage, nameof(x)));
+            }
+
+            if (y < MinValue || y > MaxValue)
+            {
+                return GeneralErrors.ValueIsInvalid(string.Format(ErrorMessage, nameof(y)));
+            }
+
+            return new Location(x, y);
+        }
+
+        /// <summary>
+        /// Создать случайную координату
+        /// </summary>
+        public static Result<Location, Error> CreateRandom()
+        {
+            var randomX = (byte)Random.Shared.Next(MinValue, MaxValue + 1);
+            var randomY = (byte)Random.Shared.Next(MinValue, MaxValue + 1);
+            return Create(randomX, randomY);
+        }
+
+        public Result<int, Error> CalculateDistance(Location to)
+        {
+            return Math.Abs(to.X - X) + Math.Abs(to.Y - Y);
+        }
+
+        [ExcludeFromCodeCoverage]
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,79 @@
+﻿using DeliveryApp.Core.Domain.Models.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.SharedKernel
+{
+    public class LocationShould
+    {
+        [Fact]
+        public void BeCorrectWhenParamsIsCorrectOnCreated()
+        {
+            //Arrange
+
+            //Act
+            var location = Location.Create(1, 2);
+
+            //Assert
+            location.Value.X.Should().Be(1);
+            location.Value.Y.Should().Be(2);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 2, 2)]
+        [InlineData(1, 1, 1, 1)]
+        [InlineData(10, 10, 10, 10)]
+        public void BeEquivalentWithSameParameters(byte testX, byte testY, byte resX, byte resY)
+        {
+            //Arrange
+            var testLocation = Location.Create(testX, testY);
+
+            //Act
+            var resultLocation = Location.Create(resX, resY);
+
+            //Assert
+            Assert.Equal(testLocation, resultLocation);
+        }
+
+        [Theory]
+        [InlineData(2, 6, 4, 9, 5)]
+        [InlineData(2, 6, 4, 10, 6)]
+        [InlineData(1, 1, 10, 10, 18)]
+        [InlineData(1, 1, 1, 1, 0)]
+        [InlineData(10, 10, 1, 1, 18)]
+        [InlineData(8, 7, 4, 2, 9)]
+        public void HarCorrectDistanceToLocation(
+            byte toX,
+            byte toY,
+            byte curX,
+            byte curY,
+            int resultDistance)
+        {
+            //Arrange
+            var toLocation = Location.Create(toX, toY);
+            var currentLocation = Location.Create(curX, curY);
+
+            //Act
+            var distance = currentLocation.Value.CalculateDistance(toLocation.Value);
+
+            //Assert
+            distance.Value.Should().Be(resultDistance);
+        }
+
+
+        [Fact]
+        public void BeCorrectRandomLocation()
+        {
+            //Arrange
+
+            //Act
+            var location = Location.CreateRandom();
+
+            //Assert
+            Assert.True(location.Value.X >= 1);
+            Assert.True(location.Value.X <= 10);
+            Assert.True(location.Value.Y >= 1);
+            Assert.True(location.Value.Y <= 10);
+        }
+    }
+}
