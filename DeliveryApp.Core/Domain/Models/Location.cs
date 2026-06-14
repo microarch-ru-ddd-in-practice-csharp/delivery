2064bb71e1955a0dbadebc73dd6000bb38df21fc
User <arcticmonkey777@yandex.ru>
2026-06-14 17:24:08 +0200

    Задания модуль 2

---
+++ DeliveryApp.Core/Domain/Models/Location.cs
@@ -0,0 +1,52 @@
+﻿using CSharpFunctionalExtensions;
+using Errs;
+
+namespace DeliveryApp.Core.Domain.Models
+{
+    public class Location : ValueObject
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        private Location(){}
+
+        private Location(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Result<Location, Error> Create(int x, int y)
+        {
+            if (x is < 1 or > 10 )
+                return GeneralErrors.ValueMustBeBetween<int>("x",  x, 1, 10 );
+            if (y is < 1 or > 10 )
+                return GeneralErrors.ValueMustBeBetween<int>("y", y, 1, 10 );
+            return new Location(x, y);
+        }
+
+        public static Location CreateRandom()
+        {
+            var randX = Random.Shared.Next(1, 11);
+            var randY = Random.Shared.Next(1, 11);
+            var randomLoc = new Location(randX, randY);
+            return randomLoc;
+        }
+        
+        public Result<int, Error> DistanceTo(Location target)
+        {
+            if (target == null)
+                return GeneralErrors.ValueIsRequired(nameof(target));
+            var myX = X;
+            var myY = Y;
+            var distance = Math.Abs(target.X - myX) + Math.Abs(target.Y - myY);
+            return distance;
+        }
+        
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,107 @@
+﻿using DeliveryApp.Core.Domain.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.SharedKernel;
+
+public class LocationShould
+{
+    [Fact]
+    public void ReturnCorrectLocation()
+    {
+        var location = Location.Create(5, 5);
+
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(5);
+        location.Value.Y.Should().Be(5);
+    }
+
+    [Theory]
+    [InlineData(0, 5)]
+    [InlineData(-1, 5)]
+    [InlineData(1, 15)]
+    [InlineData(2, -15)]
+    [InlineData(11, 5)]
+    public void CreateWithValueOutOfRangeReturnsFailure(int xValue, int yValue)
+    {
+        var location = Location.Create(xValue, yValue);
+        
+        location.IsSuccess.Should().BeFalse();
+        location.Error.Code.Should().Be("value.must.be.between");
+    }
+    
+    [Fact]
+    public void BeEqualLocationWhenEqual()
+    {
+        var location1 = Location.Create(5, 5);
+        var location2 = Location.Create(5, 5);
+
+        var result = location1.Equals(location2);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BeNotEqualLocationWhenNotEqual()
+    {
+        var location1 = Location.Create(5, 5);
+        var location2 = Location.Create(6, 6);
+        var result = location2.Equals(location1);
+        
+        result.Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CreateRandomAlwaysReturnsValidLocation()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var location = Location.CreateRandom();
+            location.X.Should().BeInRange(1, 10);
+            location.Y.Should().BeInRange(1, 10);
+        }
+    }
+    
+    [Fact]
+    public void DistanceToReturnsValidDistance()
+    {
+        var location = Location.Create(5, 5).Value;
+        var target = Location.Create(6, 6).Value;
+        var distance = location.DistanceTo(target);
+        
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(2);
+    }
+
+    [Fact]
+    public void DistanceToBetweenExampleFromTaskReturnsFive()
+    {
+        var location = Location.Create(2, 3).Value;
+        var target = Location.Create(4, 6).Value;
+        var distance = location.DistanceTo(target);
+        
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(5);
+    }
+    
+    [Fact]
+    public void DistanceToSelfReturnZero()
+    {
+        var location = Location.Create(5, 5).Value;
+        var target = Location.Create(5, 5).Value;
+        var distance = location.DistanceTo(target);
+        
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(0);
+    }
+
+    [Fact]
+    public void DistanceToWithNullTargetReturnsFailure()
+    {
+        var location = Location.Create(5, 5).Value;
+        var distance = location.DistanceTo(null!);
+        
+        distance.IsSuccess.Should().BeFalse();
+        distance.Error.Code.Should().Be("value.must.be.provided");
+    }
+    
+}
